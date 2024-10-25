83e437fa18741c3f476d14491d7fa5917645ce1c
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-10-25 17:19:50 +0530

    added

---
+++ src/main/java/com/example/employee/facade/EmployeeFacade.java
@@ -0,0 +1,106 @@
+package com.example.employee.facade;
+
+import com.example.employee.dto.EmployeesRequestDto;
+import com.example.employee.dto.EmployeesResponseDto;
+import com.example.employee.entity.Employees;
+import com.example.employee.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+@Component
+public class EmployeeFacade {
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+    public EmployeesResponseDto createUser(EmployeesRequestDto reqDto) {
+        Employees employees = reqDto.getEmployees();
+        if (employees.getEmployeeId().length() != 3) {
+            return new EmployeesResponseDto("Id must be exactly 3 digits");
+        }
+        if (employees.getName().matches("\\d+")||employees.getName().isEmpty()) {
+            return new EmployeesResponseDto("Give a proper name");
+        }
+        if (employees.getDepartment().matches("\\d+")||employees.getDepartment().isEmpty()) {
+            return new EmployeesResponseDto("Give a proper value to Department.");
+        }
+        if (employees.getDesignation().isEmpty()||employees.getDesignation().matches("\\d+")) {
+            return new EmployeesResponseDto("Give a proper value to Designation");
+        }
+        if (employees.getSalary().isEmpty()) {
+            return new EmployeesResponseDto("Employee Salary is empty");
+        }
+        try {
+            Integer.parseInt(employees.getSalary());
+        } catch (NumberFormatException e) {
+            return new EmployeesResponseDto("Employee Salary should be integer");
+        }
+        if (employees.getJoiningDate() == null) {
+            return new EmployeesResponseDto("Employee Joining Date is empty");
+        }
+        Date currentDate = new Date(); // This gives the current date and time
+        if (employees.getJoiningDate().after(currentDate)) {
+            return new EmployeesResponseDto("This Date is not valid");
+        }
+        try {
+            Integer employeeId = Integer.parseInt(employees.getEmployeeId());
+            if (employeesRepository.existsById(String.valueOf(employeeId))) {
+                return new EmployeesResponseDto("Error: Employee_Id already exists");
+            }
+        } catch (NumberFormatException e) {
+            return new EmployeesResponseDto("Id must be an integer");
+        }
+        try {
+            employeesRepository.save(employees);
+            return new EmployeesResponseDto("Employee Details Added Successfully");
+        } catch (Exception e) {
+            return new EmployeesResponseDto("Error: " + e.getMessage());
+        }
+    }
+    public String updateEmployeeById(String employeeId, Employees updatedEmploees) {
+        Optional<Employees> employeeOptional = employeesRepository.findById(employeeId);
+        if (employeeOptional.isPresent()) {
+            Employees existingEmployee = employeeOptional.get();
+            if(updatedEmploees.getName().length()<3) {
+                throw new RuntimeException(" Error: Write Your Full Name");
+            }
+            if ( updatedEmploees.getEmployeeId().isEmpty()) {
+                return "Employee ID is empty";
+            }
+            if ( updatedEmploees.getName().isEmpty()||updatedEmploees.getName().matches("\\d+")) {
+                return "Give a proper name";
+            }
+            if ( updatedEmploees.getDepartment().isEmpty()||updatedEmploees.getDepartment().matches("\\d+")) {
+                return "Give a proper value to department";
+            }
+            if (updatedEmploees.getDesignation().isEmpty()||updatedEmploees.getDesignation().matches("\\d+")) {
+                return "Give a proper value to designation";
+            }
+            if (updatedEmploees.getSalary().isEmpty()) {
+                return "Employee Salary is empty";
+            }
+            try {
+                Integer.parseInt(updatedEmploees.getSalary());
+            } catch (NumberFormatException e) {
+                return "Employee Salary should be integer";
+            }
+            if (updatedEmploees.getJoiningDate() == null) {
+                return "Employee Joining Date is empty";
+            }
+            Date currentDate = new Date();
+            if (updatedEmploees.getJoiningDate().after(currentDate)) {
+                return "This date is not valid";
+            }
+            existingEmployee.setName(updatedEmploees.getName());
+            existingEmployee.setDepartment(updatedEmploees.getDepartment());
+            existingEmployee.setDesignation(updatedEmploees.getDesignation());
+            existingEmployee.setSalary(updatedEmploees.getSalary());
+            existingEmployee.setJoiningDate(updatedEmploees.getJoiningDate());
+            employeesRepository.save(existingEmployee);
+            return "Employee details updated successfully.";
+        } else {
+            throw new RuntimeException("Employee with ID " + employeeId + " is not found.");
+        }
+    }
+}
